3e6925cf479ae2f8ba6f6269f465f92311a00e51
tsuzuzuzu <szkt1231@gmail.com>
2020-07-25 16:13:04 +0900

    解説のためのコメントを追記

--- postSlackForReminder.js
+++ postSlackForReminder.js
@@ -18,19 +18,24 @@ const SUBJECT = sp.getProperty('subject');
 // Google スプレッドシートの表から、3日追記のない場合にリマインド通知を投稿する
 function remindPostForHistoryLog() {
     try {
+        // 3日前の日付をF2セルから取得
         let _dateForCheck = dateAsParam.getRange("F2").getValue();
         let dateForCheck = Date.parse(_dateForCheck);
-        let sheetNames = dateAsParam.getRange("G2:G7").getValues();
 
+        // G2:G7にメンバー名を記載して
+        let sheetNames = dateAsParam.getRange("G2:G7").getValues();
         sheetNames.forEach(sheetName => {
             let member = sheet.getSheetByName(sheetName);
             let val = member.getRange('C:C').getValues();
             let _lastRow = val.filter(String).length;
             let lastRow = _lastRow + 1;
+
+            // スプレッドシートから最新の追記された日付を取得
             let getDate = member.getRange(lastRow, 3).getValue();
             let _inputDate = Utilities.formatDate(getDate, "Asia/Tokyo", "yyyy/MM/dd");
             let inputDate = Date.parse(_inputDate);
 
+            // 「最新の追記された日付」と「3日前」の日付を比較して結果で分岐
             if (dateForCheck <= inputDate) {
                 let result = sheetName + ", good!";
                 Logger.log(result);
@@ -42,7 +47,7 @@ function remindPostForHistoryLog() {
         });
     } catch (err) {
         console.log(err);
-        // Slackにメッセージを送る
+        // GmailからSlackへ投稿（EmailAppのSlackに付与されたEmailアドレスを利用）
         let = postToSlack = "remindPostForHistoryLog\n" + err.message;
         GmailApp.sendEmail(TO, SUBJECT, postToSlack);
         throw (err);
@@ -61,7 +66,7 @@ function notifySlack(result) {
     };
     let options = {
         'method': 'post',
-        'contentType': 'application/json',
+        'contentType': 'application/json; charset=UTF-8',
         'payload': JSON.stringify(payload)
     };
 
